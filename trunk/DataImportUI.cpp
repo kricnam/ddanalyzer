c09be0d1b62b0c012209d1c76850952212f64786
me.xinxin@gmail.com <me.xinxin@gmail.com@0c99938a-cc01-a779-93b5-7c2b290c5a91>
2012-11-28 14:55:57 +0000

    new UI

---
+++ trunk/DataImportUI.cpp
@@ -0,0 +1,24 @@
+/*
+ * DataImportUI.cpp
+ *
+ *  Created on: Nov 28, 2012
+ *      Author: mxx
+ */
+
+#include "DataImportUI.h"
+
+namespace DataCenter
+{
+
+DataImportUI::DataImportUI()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+DataImportUI::~DataImportUI()
+{
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace DataCenter */
